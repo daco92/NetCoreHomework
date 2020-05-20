07a8b4ddb56385d6d1e60f10aaad4e5c29d48e45
Robert <daco.chen@gmail.com>
2020-05-20 14:23:06 +0800

    重新產生poko code

--- Models/ContosouniversityContext.cs
+++ Models/ContosouniversityContext.cs
@@ -22,169 +22,122 @@ namespace NetCoreHomework.Models
         public virtual DbSet<Enrollment> Enrollment { get; set; }
         public virtual DbSet<OfficeAssignment> OfficeAssignment { get; set; }
         public virtual DbSet<Person> Person { get; set; }
-        public virtual DbSet<vwCourseStudentCount> vwCourseStudentCount { get; set; }
-        public virtual DbSet<vwCourseStudents> vwCourseStudents { get; set; }
-        public virtual DbSet<vwDepartmentCourseCount> vwDepartmentCourseCount { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ContosoUniversity;Integrated Security=True");
-            }
-        }
+        public virtual DbSet<VwCourseStudentCount> VwCourseStudentCount { get; set; }
+        public virtual DbSet<VwCourseStudents> VwCourseStudents { get; set; }
+        public virtual DbSet<VwDepartmentCourseCount> VwDepartmentCourseCount { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>(entity =>
             {
-                entity.HasIndex(e => e.DepartmentID)
+                entity.HasIndex(e => e.DepartmentId)
                     .HasName("IX_DepartmentID");
 
-                entity.Property(e => e.DepartmentID).HasDefaultValueSql("((1))");
-
-                entity.Property(e => e.Title).HasMaxLength(50);
+                entity.Property(e => e.DepartmentId).HasDefaultValueSql("((1))");
 
                 entity.HasOne(d => d.Department)
                     .WithMany(p => p.Course)
-                    .HasForeignKey(d => d.DepartmentID)
+                    .HasForeignKey(d => d.DepartmentId)
                     .HasConstraintName("FK_dbo.Course_dbo.Department_DepartmentID");
             });
 
             modelBuilder.Entity<CourseInstructor>(entity =>
             {
-                entity.HasKey(e => new { e.CourseID, e.InstructorID })
+                entity.HasKey(e => new { e.CourseId, e.InstructorId })
                     .HasName("PK_dbo.CourseInstructor");
 
-                entity.HasIndex(e => e.CourseID)
+                entity.HasIndex(e => e.CourseId)
                     .HasName("IX_CourseID");
 
-                entity.HasIndex(e => e.InstructorID)
+                entity.HasIndex(e => e.InstructorId)
                     .HasName("IX_InstructorID");
 
                 entity.HasOne(d => d.Course)
                     .WithMany(p => p.CourseInstructor)
-                    .HasForeignKey(d => d.CourseID)
+                    .HasForeignKey(d => d.CourseId)
                     .HasConstraintName("FK_dbo.CourseInstructor_dbo.Course_CourseID");
 
                 entity.HasOne(d => d.Instructor)
                     .WithMany(p => p.CourseInstructor)
-                    .HasForeignKey(d => d.InstructorID)
+                    .HasForeignKey(d => d.InstructorId)
                     .HasConstraintName("FK_dbo.CourseInstructor_dbo.Instructor_InstructorID");
             });
 
             modelBuilder.Entity<Department>(entity =>
             {
-                entity.HasIndex(e => e.InstructorID)
+                entity.HasIndex(e => e.InstructorId)
                     .HasName("IX_InstructorID");
 
-                entity.Property(e => e.Budget).HasColumnType("money");
-
-                entity.Property(e => e.Name).HasMaxLength(50);
-
                 entity.Property(e => e.RowVersion)
-                    .IsRequired()
                     .IsRowVersion()
                     .IsConcurrencyToken();
 
-                entity.Property(e => e.StartDate).HasColumnType("datetime");
-
                 entity.HasOne(d => d.Instructor)
                     .WithMany(p => p.Department)
-                    .HasForeignKey(d => d.InstructorID)
+                    .HasForeignKey(d => d.InstructorId)
                     .HasConstraintName("FK_dbo.Department_dbo.Instructor_InstructorID");
             });
 
             modelBuilder.Entity<Enrollment>(entity =>
             {
-                entity.HasIndex(e => e.CourseID)
+                entity.HasIndex(e => e.CourseId)
                     .HasName("IX_CourseID");
 
-                entity.HasIndex(e => e.StudentID)
+                entity.HasIndex(e => e.StudentId)
                     .HasName("IX_StudentID");
 
                 entity.HasOne(d => d.Course)
                     .WithMany(p => p.Enrollment)
-                    .HasForeignKey(d => d.CourseID)
+                    .HasForeignKey(d => d.CourseId)
                     .HasConstraintName("FK_dbo.Enrollment_dbo.Course_CourseID");
 
                 entity.HasOne(d => d.Student)
                     .WithMany(p => p.Enrollment)
-                    .HasForeignKey(d => d.StudentID)
+                    .HasForeignKey(d => d.StudentId)
                     .HasConstraintName("FK_dbo.Enrollment_dbo.Person_StudentID");
             });
 
             modelBuilder.Entity<OfficeAssignment>(entity =>
             {
-                entity.HasKey(e => e.InstructorID)
+                entity.HasKey(e => e.InstructorId)
                     .HasName("PK_dbo.OfficeAssignment");
 
-                entity.HasIndex(e => e.InstructorID)
+                entity.HasIndex(e => e.InstructorId)
                     .HasName("IX_InstructorID");
 
-                entity.Property(e => e.InstructorID).ValueGeneratedNever();
-
-                entity.Property(e => e.Location).HasMaxLength(50);
+                entity.Property(e => e.InstructorId).ValueGeneratedNever();
 
                 entity.HasOne(d => d.Instructor)
                     .WithOne(p => p.OfficeAssignment)
-                    .HasForeignKey<OfficeAssignment>(d => d.InstructorID)
+                    .HasForeignKey<OfficeAssignment>(d => d.InstructorId)
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("FK_dbo.OfficeAssignment_dbo.Instructor_InstructorID");
             });
 
             modelBuilder.Entity<Person>(entity =>
             {
-                entity.Property(e => e.Discriminator)
-                    .IsRequired()
-                    .HasMaxLength(128)
-                    .HasDefaultValueSql("('Instructor')");
-
-                entity.Property(e => e.EnrollmentDate).HasColumnType("datetime");
-
-                entity.Property(e => e.FirstName)
-                    .IsRequired()
-                    .HasMaxLength(50);
-
-                entity.Property(e => e.HireDate).HasColumnType("datetime");
-
-                entity.Property(e => e.LastName)
-                    .IsRequired()
-                    .HasMaxLength(50);
+                entity.Property(e => e.Discriminator).HasDefaultValueSql("('Instructor')");
             });
 
-            modelBuilder.Entity<vwCourseStudentCount>(entity =>
+            modelBuilder.Entity<VwCourseStudentCount>(entity =>
             {
                 entity.HasNoKey();
 
                 entity.ToView("vwCourseStudentCount");
-
-                entity.Property(e => e.Name).HasMaxLength(50);
-
-                entity.Property(e => e.Title).HasMaxLength(50);
             });
 
-            modelBuilder.Entity<vwCourseStudents>(entity =>
+            modelBuilder.Entity<VwCourseStudents>(entity =>
             {
                 entity.HasNoKey();
 
                 entity.ToView("vwCourseStudents");
-
-                entity.Property(e => e.CourseTitle).HasMaxLength(50);
-
-                entity.Property(e => e.DepartmentName).HasMaxLength(50);
-
-                entity.Property(e => e.StudentName).HasMaxLength(101);
             });
 
-            modelBuilder.Entity<vwDepartmentCourseCount>(entity =>
+            modelBuilder.Entity<VwDepartmentCourseCount>(entity =>
             {
                 entity.HasNoKey();
 
                 entity.ToView("vwDepartmentCourseCount");
-
-                entity.Property(e => e.Name).HasMaxLength(50);
             });
 
             OnModelCreatingPartial(modelBuilder);
