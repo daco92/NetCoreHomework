e0693a48dfa368f7fc4cd028ff92b12fe33a8fc9
dacochen <daco.chen@gmail.com>
2020-05-21 17:25:03 +0800

    針對 Departments 表格的 CUD 操作需用到預存程序

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using NetCoreHomework.Models;
 
 namespace NetCoreHomework.Controllers {
@@ -42,19 +44,66 @@ namespace NetCoreHomework.Controllers {
         }
 
         // POST api/department
-        [HttpPost ("")]
-        public void PostDepartment (Department value) { }
+        [HttpPost ("add")]
+        public void PostDepartment (Department value) {
+            var Entity = new Department () {
+                Name = value.Name,
+                Budget = value.Budget,
+                StartDate = value.StartDate,
+                InstructorId = value.InstructorId
+            };
+
+            SqlParameter Name = new SqlParameter ("@Name", Entity.Name);
+            SqlParameter Budget = new SqlParameter ("@Budget", Entity.Budget);
+            SqlParameter StartDate = new SqlParameter ("@StartDate", Entity.StartDate);
+            SqlParameter InstructorId = new SqlParameter ("@InstructorId", Entity.InstructorId);
+            db.Database.ExecuteSqlRaw (" exec Department_Insert @Name,@Budget, @StartDate ,@InstructorId", Name, Budget, StartDate, InstructorId);
+
+        }
 
         // PUT api/department/5
         [HttpPut ("{id}")]
-        public void PutDepartment (int id, Department value) { }
+        public void PutDepartment (int id, Department value) {
+            // var Entitiy = db.Department.Find (id);
+            // Entitiy.Name = value.Name;
+            // Entitiy.Budget = value.Budget;
+            // Entitiy.StartDate = value.StartDate;
+            // Entitiy.InstructorId = value.InstructorId;
+            // db.SaveChanges();
+
+            var model = db.Department.Find(id);
+            var Entity = new Department () {
+                DepartmentId = id,
+                Name = value.Name,
+                Budget = value.Budget,
+                StartDate = value.StartDate,
+                InstructorId = value.InstructorId,
+                RowVersion= model.RowVersion
+            };
+            
+            SqlParameter DepartmentId = new SqlParameter ("@DepartmentID", Entity.DepartmentId);
+            SqlParameter Name = new SqlParameter ("@Name", Entity.Name);
+            SqlParameter Budget = new SqlParameter ("@Budget", Entity.Budget);
+            SqlParameter StartDate = new SqlParameter ("@StartDate", Entity.StartDate);
+            SqlParameter InstructorId = new SqlParameter ("@InstructorId", Entity.InstructorId);
+            SqlParameter RowVersion_Original = new SqlParameter ("@RowVersion_Original", Entity.RowVersion);
+            db.Database.ExecuteSqlRaw (" exec Department_Update @DepartmentID,@Name,@Budget, @StartDate ,@InstructorId,@RowVersion_Original", 
+                                            DepartmentId,Name, Budget, StartDate, InstructorId,RowVersion_Original);
+
+        }
 
         // DELETE api/department/5
         [HttpDelete ("{id}")]
         public void DeleteDepartmentById (int id) {
-            var model = db.Department.Find (id);
-            db.Department.Remove (model);
-            db.SaveChanges ();
+            var Entity = db.Department.Find (id);
+            // db.Department.Remove (model);
+            // db.SaveChanges ();
+
+            SqlParameter departmentID = new SqlParameter ("@DepartmentID", Entity.DepartmentId);
+            SqlParameter rowVersion_Original = new SqlParameter ("@RowVersion_Original", Entity.RowVersion);
+
+            db.Database.ExecuteSqlRaw (" exec Department_Delete @DepartmentID, @RowVersion_Original", departmentID, rowVersion_Original);
+
         }
     }
 }
